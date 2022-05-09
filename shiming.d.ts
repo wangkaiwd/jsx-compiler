c2687a3702d44ed130fa7e203e4213d36e379eaf
wangkaiwd <1162486176@qq.com>
2022-05-10 01:09:32 +0800

    chore: try break down babel parse stage but fail

---
+++ shiming.d.ts
@@ -0,0 +1,3 @@
+declare module '@babel/plugin-transform-react-jsx' {
+  export default any;
+}
---
+++ test.js
@@ -0,0 +1,21 @@
+import * as babel from '@babel/core'; // transform
+// 1. babel plugin execute opportunity
+// 2. parse jsx
+// Stages of Babel: source -> parse -> transform -> generate
+// https://github.com/babel/babel/blob/main/CONTRIBUTING.md#contributing
+const code = `
+  const App = () => {
+    const obj = {a:1,b:2}
+    return <div className="test" {...obj}>hello jsx</div>
+  }
+`;
+
+const result = babel.transformSync(code, { plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]] });
+console.log('result', result);
+// babel plugin:
+//  plugin type: https://babeljs.io/docs/en/options#entrytarget
+//   function or object
+//   function: return object which own visitor property
+//   object: own visitor property
+
+
